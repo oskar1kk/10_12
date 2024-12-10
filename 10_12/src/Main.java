cf9847b22ca40c33eac60a542be3544b681efb03
oskar1kk <oskarsilishko123123@gmail.com>
2024-12-10 09:30:02 +0200

    Add files via upload

---
+++ 10_12/src/Main.java
@@ -0,0 +1,35 @@
+public class Main {
+    public static void main(String[] args) {
+        func(23,74);
+        func(17,24);
+        func(17,54);
+
+    }
+    public static void func (int a, int b) {
+        int res = a + b;
+        System.out.println("summa ir " + res);
+    }
+    public static void batter() {
+        //        1	Make the cookie batter.
+        System.out.println("Mix the dry ingredients.");
+        System.out.println("Cream the butter and sugar.");
+        System.out.println("Beat in the eggs.");
+        System.out.println("Stir in the dry ingredients.");
+    }
+    public static void baking() {
+        //        2	Bake the cookies.
+        System.out.println("Set the oven temperature.");
+        System.out.println("Set the timer.");
+        System.out.println("Place a batch of cookies into the oven.");
+        System.out.println("Allow the cookies to bake.");
+        System.out.println("Set the oven temperature.");
+        System.out.println("Set the timer.");
+        System.out.println("Place a batch of cookies into the oven.");
+        System.out.println("Allow the cookies to bake.");
+    }
+    public static void decorate() {
+        //        3	Add frosting and sprinkles.
+        System.out.println("Mix ingredients for frosting.");
+        System.out.println("Spread frosting and sprinkles.");
+    }
+}
